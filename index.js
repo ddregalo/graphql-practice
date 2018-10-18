128d10975e8a57638cfcfe09e5d78139c187bfcb
danielcampos <ddregalo@gmail.com>
2018-10-18 23:56:22 +0100

    Create app server port and get root endpoint

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import express from 'express';
 
 const app = express();
+
+app.get('/', (req, res, err) => {
+    res.send("GraphQL Testing 1-2 1-2 ");
+});
+
+app.listen(8080, () => console.log("Running server on port localhost:8080/graphql"));
